83b5d02f5a8ffd4de26824ca70e42d6354cba24b
innihe <insert.email.here@web.de>
2021-01-13 13:45:09 +0100

    v2 with fancy bug

--- Code/ledrace/src/main.cpp
+++ Code/ledrace/src/main.cpp
@@ -2,7 +2,7 @@
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
-#define PIN        2
+#define PIN        32
 #define NUMPIXELS 300
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
@@ -13,8 +13,11 @@ void setup() {
   clock_prescale_set(clock_div_1);
 #endif
 
+  Serial.begin(115200);
+  Serial.println("debug setup");
   pixels.begin();
 
+  pixels.clear();
   //draw player 1 start position
   pixels.setPixelColor(0, pixels.Color(0, 150, 0));
   pixels.setPixelColor(1, pixels.Color(0, 150, 0));
@@ -29,36 +32,44 @@ int player1CurrentPosition = 2;
 //Draw players
 void draw(int playerLastPosition, int playerCurrentPosition)
 {
+  Serial.println("debug draw");
   int playerLastPositionLocal = playerLastPosition;
   //While player real location and player drawn location differ
   while(playerLastPositionLocal != playerCurrentPosition)
   {
+    Serial.println("debug draw while");
     //if player real position is in front of current drawn position
     if(playerLastPositionLocal < playerCurrentPosition)
     {
+      Serial.println("debug draw while if");
       //unset last entity pixel
-      int lastEntityPixel = playerLastPosition - 2;
+      int lastEntityPixel = playerLastPositionLocal - 2;
       pixels.setPixelColor(lastEntityPixel, pixels.Color(0, 0, 0));
 
-       //set next pixels
-       int nextEntityPixel = playerLastPosition + 1;
+       //set next pixel
+       int nextEntityPixel = playerLastPositionLocal + 1;
        pixels.setPixelColor(nextEntityPixel, pixels.Color(0, 150, 0));
+
 
        pixels.show();
        //update playerLastPosisition
        playerLastPositionLocal++;
     }
+    //remove this line for fancy bug
+    //player1LastPosition = playerLastPositionLocal;
   }
 }
 
 void update()
 {
-  delay(300);
+  Serial.println("debug update"+player1CurrentPosition);
+  delay(50);
   player1CurrentPosition++;
 }
 
 void loop()
 {
+  Serial.println("debug loop");
   draw(player1LastPosition, player1CurrentPosition);
   update();
 
