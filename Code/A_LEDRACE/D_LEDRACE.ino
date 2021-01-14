07591a8014712a2e3120bfbaaecb2d9b9e969bb3
FloriansCode <70821651+FloriansCode@users.noreply.github.com>
2021-01-14 14:18:38 +0100

    Remote Upload Dennis MenüGrundeinstellungen

--- Code/A_LEDRACE/D_LEDRACE.ino
+++ Code/A_LEDRACE/D_LEDRACE.ino
@@ -1,10 +1,10 @@
 
-  
+
 void Read()
 {
-    a = digitalRead(19);
-    b = digitalRead(18);
-    c = digitalRead(5);
+  a = digitalRead(19);
+  b = digitalRead(18);
+  c = digitalRead(5);
   //  d = digitalRead(3);
   //  e = digitalRead(4);
   //  f = digitalRead(5);
@@ -13,7 +13,37 @@ void Read()
 void abfrage()
 {
   Read();
-  if (a == 1){ Serial.println(2);}
-  else if (b == 1){ Serial.println(0);}
-  else if (c == 1){ Serial.println(4);}
+  if      (a == 1) {
+    m = m + 1;
+  }
+  else if (b == 1) {
+    m = m - 1;
+  }
+  else if (c == 1) {
+    w = w + 1;
+  }
+  Serial.println(m);
+  Serial.println(s);
+}
+
+void abfrage2()
+{
+  Read();
+  if (a == 1) {
+    s = 0;
+  }
+  Read();
+  if (b == 1) {
+    s = 1;
+  }
+  Read();
+  if (c == 1 && s == 1) {
+    w = w + 1;
+  }
+  Read();
+  if (c == 1 && s == 0) {
+    w = w - 1;
+  }
+  Serial.println(m);
+  Serial.println(s);
 }
